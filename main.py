ce87a76e50aae7f541e81205f007ed5b1bc86082
alexiahudea2003 <91561462+alexiahudea2003@users.noreply.github.com>
2021-10-10 17:15:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Proprietatile 7 si 17
+
 def print_menu():
     print("1. Citire lista")
     print("2. Determinare cea mai lungă subsecvență cu proprietatea 1")
@@ -12,13 +14,12 @@ def citire_lista():
         l.append(int(x))
     return l
 
-def medie(l):
-    sum(l)/len(l)
-    x=int(input("Dati numarul:"))
-    if medie<x:
-        return True
-
 def is_prime(x):
+    '''
+    determina daca un nr. este prim
+    :param x: un numar intreg
+    :return: True, daca x este prim sau False in caz contrar
+    '''
     if x < 2:
         return False
     for i in range(2, x//2 + 1):
@@ -27,59 +28,89 @@ def is_prime(x):
     return True
 
 def test_is_prime():
-    assert is_prime(0) == False
-    assert is_prime(1) == False
-    assert is_prime(5) == True
+    assert is_prime(0) is False
+    assert is_prime(1) is False
+    assert is_prime(5) is True
 
-def not_prime(x):
-    if is_prime(x) == True:
-        return False
-    elif is_prime(x) == False:
-        return True
+def not_prime(l):
+    '''
+    determina daca toate elementele unei liste sunt neprime
+    :param l: lista nr. intregi
+    :return: True, daca proprietatea e adevarata, False in caz contrar
+    '''
+    for x in l:
+        if is_prime(x):
+            return False
+    return True
 
 def test_not_prime():
-    assert not_prime(7) == False
-    assert not_prime(8) == True
-    assert not_prime(9) == True
-
-def toate_elementele_neprime(l):
-    rezultat = []
-    for x in l:
-        if not_prime(x) == True:
-            rezultat.append(x)
-    return rezultat
-
-def test_toate_elementele_neprime():
-    assert toate_elementele_neprime([]) == []
-    assert toate_elementele_neprime([3,4]) == [4]
-    assert toate_elementele_neprime([6,9]) == [6,9]
+    assert not_prime([19,6,8]) is False
+    assert not_prime([7,13]) is False
+    assert not_prime([44,40,12]) is True
 
 def get_longest_all_not_prime(l):
-    subsecventa_max = []
+    '''
+    determina cea mai lunga subsecventa de nr. neprime dintr-o lista
+    :param l: lista de nr. intregi
+    :return: cea mai lunga subsecventa de nr. neprime
+    '''
+    secventa_finala = []
+    lungime_maxima = 0
     for i in range(len(l)):
         for j in range(i, len(l)):
-            if toate_elementele_neprime(l[i:j+1]) and len(subsecventa_max) < len(l[i:j+1]):
-                subsecventa_max = l[i:j+1]
-    return subsecventa_max
+            secventa_initiala = l[i:j+1]
+            if len(secventa_initiala) > lungime_maxima and not_prime(secventa_initiala):
+                lungime_maxima = len(secventa_initiala)
+                secventa_finala = secventa_initiala
+    return secventa_finala
 
 def test_get_longest_all_not_prime():
-    assert get_longest_all_not_prime([]) == []
-    assert get_longest_all_not_prime([2,3]) == []
-    assert get_longest_all_not_prime([10,20])== [10,20]
+    assert get_longest_all_not_prime([6,8,7]) == [6,8]
+    assert get_longest_all_not_prime([3,6,7]) == [6]
+    assert get_longest_all_not_prime([6,8,10,7]) == [6,8,10]
 
-def get_longest_average_below(l):
-    subsecventa_max = []
+valoare=int(input("Introduceti valoarea:"))
+
+def medie_inferioara_unei_valori(l,valoare):
+    '''
+    determina daca media aritmetica a elementelor unei liste este mai mica decat o valoare data
+    :param l: lista de nr. intregi
+    :return: True sau False
+    '''
+    if(sum(l)/len(l) < valoare == True):
+        return True
+    else:
+        return False
+
+def test_medie_inferioara_unei_valori():
+    assert medie_inferioara_unei_valori([7,8],12) is False
+    assert medie_inferioara_unei_valori([10,20],20) is True
+    assert medie_inferioara_unei_valori([3,2],6) is False
+
+def get_longest_average_below(l,valoare):
+    '''
+    determina cea mai lunga subsecventa de nr. a caror medie aritmetica se afla sub valoarea data
+    :param l: lista de nr. intregi
+    :return: cea mai lunga subsecventa de nr. a caror medie aritmetica se afla sub valoarea data
+    '''
+    secventa_finala = []
+    lungime_maxima = 0
     for i in range(len(l)):
         for j in range(i, len(l)):
-            if medie(l[i:j + 1]) and len(subsecventa_max) < len(l[i:j + 1]):
-                subsecventa_max = l[i:j + 1]
-    return subsecventa_max
+            secventa_initiala = l[i:j+1]
+            if len(secventa_initiala) > lungime_maxima and medie_inferioara_unei_valori(secventa_initiala,valoare):
+                lungime_maxima = len(secventa_initiala)
+                secventa_finala = secventa_initiala
+    return secventa_finala
+
+def test_get_longest_average_below():
+    assert get_longest_average_below([6,8,22],8) == [6,8]
+    assert get_longest_average_below([],3) == []
+    assert get_longest_average_below([3,9],5) == [3]
 
 def main():
-    test_is_prime()
-    test_not_prime()
-    test_toate_elementele_neprime()
     test_get_longest_all_not_prime()
+    test_get_longest_average_below
     l = []
     while True:
         print_menu()
@@ -89,8 +120,7 @@ def main():
         elif optiune == "2":
             print(get_longest_all_not_prime(l))
         elif optiune == "3":
-            print(get_longest_average_below(l))
+            print(get_longest_average_below(l,valoare))
         elif optiune == "4":
             break
-
 main()
